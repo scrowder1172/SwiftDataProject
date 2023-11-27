fd57a77813c6a1c52a16840e59606c4fdf371058
Scott Crowder <scrowder@gmail.com>
2023-11-27 10:21:04 -0600

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by SCOTT CROWDER on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
