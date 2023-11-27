e733bd93498eb54525c2bf7683f954a46454158a
Scott Crowder <scrowder@gmail.com>
2023-11-27 11:55:24 -0600

    add file

---
+++ SwiftDataProject/TutorialFiles/SampleUsersView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleUsersView.swift
+//  SwiftDataProject
+//
+//  Created by SCOTT CROWDER on 11/27/23.
+//
+
+import SwiftUI
+
+struct SampleUsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SampleUsersView()
+}
