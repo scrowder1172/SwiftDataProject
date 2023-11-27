6845870ad4085db708b1d94d2253dc66b41f864a
Scott Crowder <scrowder@gmail.com>
2023-11-27 14:07:42 -0600

    add file

---
+++ SwiftDataProject/TutorialFiles/DynamicListView.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicListView.swift
+//  SwiftDataProject
+//
+//  Created by SCOTT CROWDER on 11/27/23.
+//
+
+import SwiftUI
+
+struct DynamicListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DynamicListView()
+}
