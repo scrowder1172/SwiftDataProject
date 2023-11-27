a1bf07cf14c59b1283dc1d11f305cc2da7ea2687
Scott Crowder <scrowder@gmail.com>
2023-11-27 11:13:19 -0600

    create Users view to add new users

---
+++ SwiftDataProject/TutorialFiles/Users.swift
@@ -0,0 +1,40 @@
+//
+//  Users.swift
+//  SwiftDataProject
+//
+//  Created by SCOTT CROWDER on 11/27/23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct Users: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \User.name) var users: [User]
+    @State private var path = [User]()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            List(users) { user in
+                NavigationLink(value: user) {
+                    Text(user.name)
+                }
+            }
+            .navigationTitle("Users")
+            .navigationDestination(for: User.self) { user in
+                EditUserView(user: user)
+            }
+            .toolbar {
+                Button("Add User", systemImage: "person.crop.circle.badge.plus") {
+                    let user: User = User(name: "", city: "", joinDate: .now)
+                    modelContext.insert(user)
+                    path = [user]
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    Users()
+}
