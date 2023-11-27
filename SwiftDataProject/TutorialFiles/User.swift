406c8f6aca23dbf186fc018390b7a634320f48ea
Scott Crowder <scrowder@gmail.com>
2023-11-27 10:53:18 -0600

    create user class

--- SwiftDataProject/TutorialFiles/User.swift
+++ SwiftDataProject/TutorialFiles/User.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model final class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
