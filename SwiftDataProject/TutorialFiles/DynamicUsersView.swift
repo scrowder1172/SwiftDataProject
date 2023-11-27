cbe5d702e63e9fd30441cffe0eb09b9d34d0554f
Scott Crowder <scrowder@gmail.com>
2023-11-27 13:56:11 -0600

    add file

---
+++ SwiftDataProject/TutorialFiles/DynamicUsersView.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicUsersView.swift
+//  SwiftDataProject
+//
+//  Created by SCOTT CROWDER on 11/27/23.
+//
+
+import SwiftUI
+
+struct DynamicUsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DynamicUsersView()
+}
